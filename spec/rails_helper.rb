96ad40538f500534cd8e273acd6eff37f45ef531
Steve Allen <sallen@amberstyle.ca>
2017-11-07 17:45:22 -0800

    Using CircleCI new report uploader

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,19 @@
 require 'simplecov'
-SimpleCov.start
+
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start do
+  add_filter '/vendor/'
+  add_filter '/config/'
+  add_filter '/spec/'
+  add_filter '/db/'
+  add_filter '/app/mailers/'
+  add_filter '/app/helpers/'
+  add_filter '/app/uploaders/'
+end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
