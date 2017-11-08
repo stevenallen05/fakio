100a0c064ce4d4026739d2b8adb72a531d3a03d0
Steve Allen <sallen@amberstyle.ca>
2017-11-07 17:47:51 -0800

    Removing pointless code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,5 @@
 require 'simplecov'
 
-if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
-  SimpleCov.coverage_dir(dir)
-end
-
 SimpleCov.start do
   add_filter '/vendor/'
   add_filter '/config/'
